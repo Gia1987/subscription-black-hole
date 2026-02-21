7ba6167822c9a87d15c31fb30f50b27fcd0bca55
Giacomo <giacomo.n27@gmail.com>
2026-02-21 13:36:29 +0000

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./Home/Home";
-import Animation from "./components/solarSystem/SolarSystem";
+import { Animation } from "./components/solarSystem/SolarSystem";
 
 function App() {
   return (
